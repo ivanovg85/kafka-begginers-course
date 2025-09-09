9448cd73e4723c7ca77d4cf2cdafcb9ceb1ddd87
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-09-09 22:07:56 +0300

    add compression and increase batch size

--- kafka-producer-wikimedia/src/main/java/org/example/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/example/WikimediaChangesProducer.java
@@ -22,6 +22,11 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // high throughput producer (at expense of a bit of latency and CPU usage)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
